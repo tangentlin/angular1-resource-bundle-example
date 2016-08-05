a6d1059a4db975fe6fe1c175c45829c30f14caf2
Tianzhen Lin <tangent@usa.net>
2016-08-05 17:20:46 -0400

    Implemente date localization

--- src/scripts/module.resource.bundle.js
+++ src/scripts/module.resource.bundle.js
@@ -1,30 +1,25 @@
-(function(){
+(function () {
     var module = angular.module('ngResourceBundle');
-    module.config(["$translateProvider",function($translateProvider){
+    module.config(["$translateProvider", function ($translateProvider) {
 
         var en_translations = {
-            "username" : "Username",
-            "password" : "Password",
+            "username": "Username",
+            "password": "Password",
             "login": "Login",
             "bad_login": "Bad Login",
-            "error_101" : 'Sorry, your login information is incorrect.' //login error
+            "error_101": 'Sorry, your login information is incorrect.' //login error
         };
 
-        var cn_translations = {
-            "username" : "用户名",
-            "password" : "密码",
-            "login": "成功登录",
+        var zh_translations = {
+            "username": "用户名",
+            "password": "密码",
+            "login": "登录",
             "bad_login": "模拟失败",
-            "error_101" : '对不起, 您的登陆数据出错' //login error
+            "error_101": '对不起, 您的登陆数据出错' //login error
         };
 
-
-        $translateProvider.translations('en',en_translations);
-
-        $translateProvider.translations('cn',cn_translations);
-
-        $translateProvider.preferredLanguage('en');
-
+        $translateProvider.translations('en', en_translations);
+        $translateProvider.translations('zh', zh_translations);
     }]);
 }());
 
--- src/scripts/super-greeting.component.js
+++ src/scripts/super-greeting.component.js
@@ -3,18 +3,26 @@
 
     var module = angular.module('ngResourceBundle');
 
-    function controller($translate) {
+    function controller($translate, $dynamicLocale) {
         var model = this;
 
-        model.$onInit = function() {
+        var preferLanguage = 'zh';
 
-        }
+        model.$onInit = function() {
+            model.date = new Date();
+            model.setLocale(preferLanguage);
+        };
+
+        model.setLocale = function(languageCode) {
+            $translate.preferredLanguage(languageCode);
+            $dynamicLocale.set(languageCode);
+        };
     }
 
     module.component('superGreeting', {
         templateUrl: 'templates/super-greeting.html',
         controllerAs: 'model',
-        controller: ['$translate', controller]
+        controller: ['$translate', 'tmhDynamicLocale', controller]
     });
 
 }());
